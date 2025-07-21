d9bd2678e0782a5540e23b3f9aef3f5fbfcb8a20
chengkangzai <pycck@hotmail.com>
2025-07-21 23:57:08 +0800

    fix: update aqi:check command to use CheckAqi class

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Console\Commands\CheckAqi;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -8,7 +9,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('aqi:check')
+Schedule::command(CheckAqi::class)
     ->everyFifteenMinutes()
     ->withoutOverlapping()
     ->runInBackground();
